5a0dedf4d360404cb8d020fcea39bc39ebeede9a
Pedro <pedro.oliveira.unb@gmail.com>
2020-12-31 16:46:40 -0300

    Adding coverage

--- jest.config.ts
+++ jest.config.ts
@@ -32,12 +32,9 @@ export default {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    'json-summary'
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
